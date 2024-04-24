f0df17f9176d1a966bbc8ab776273721a7abb25d
syedbadhusha <syedbadhusha21@gmail.com>
2024-04-24 11:57:19 +0530

    changed in index file and readme file

--- Readme.md
+++ Readme.md
@@ -1,10 +1,10 @@
-#Create mentor json format
+##Create mentor json format
     -
     {
         "name":"Sai Mohan",
         "yearsofexp":8
     }
-#Create Student json format
+##Create Student json format
     -
     {
     "name":"Syed Badhusha",
@@ -13,3 +13,19 @@
     "contactno":"8122569394"
 }
 
+##Write API to create Mentor
+    - End Point - /creatementor
+##Write API to create Student
+    - End Point - /createstudent
+##Write API to Assign a student to Mentor
+    - Select one mentor and Add multiple Student
+        - End point and request parameter will be mentor object id - /assingmentor/:mentorobjid
+    - A student who has a mentor should not be shown in List
+        - End point - /nonassignedstudents
+##Write API to Assign or Change Mentor for particular Student
+    - Select One Student and Assign one Mentor
+        - End point and request parameter will be Student object id - /assingonestudent/:studentobjid
+##Write API to show all students for a particular mentor
+    - End Point and request parameter will be mentor object id - /studentsundermentor/:mentorid
+##Write an API to show the previously assigned mentor for a particular student
+    - End point and request parameter will be Student object id - /assignedstudent/:studentid
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 app.listen("3000");
 app.use(express.json());
 
 const URL =
   "mongodb+srv://admin:admin123@loopacc.vzozr5w.mongodb.net/?retryWrites=true&w=majority&appName=loopacc";
-/// API to create Mentor
+/// Write API to create Mentor
 app.post("/creatementor", async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
@@ -20,7 +20,7 @@ app.post("/creatementor", async (req, res) => {
     res.status(500).json({ message: "somthing went werong" });
   }
 });
-// API To Create Student
+// Write API to create Student
 app.post("/createstudent", async (req, res) => {
   try {
     const connection = await MongoClient.connect(URL);
@@ -34,38 +34,101 @@ app.post("/createstudent", async (req, res) => {
     res.status(500).json({ message: "somthing went werong" });
   }
 });
-// Write API to assign a student to mentor
-app.put("/assingmentor/", async (req, res) => {
-    const mentorname = req.body.mentorname;
-    const students  =   req.body.students;
-    const connection = await MongoClient.connect(URL);
-    const db = connection.db("mentorandstudent");        
-  try { 
-        for (const stuname of students) {
-            const collectionstu = db.collection("students");
-            const collectionment = db.collection("mentors");
-            const mentorAvail = await collectionment.findOne({ name: mentorname });
-            const studentAvail = await collectionstu.findOne({ name: stuname });
-            if(!mentorAvail && !studentAvail){
-                await connection.close();
-                return res.status(504).json({message: `${mentorname} and ${stuname} Both are Not Available Create Before Asigning`})
-            }else if(!mentorAvail && studentAvail){
-                await connection.close();
-                return res.status(504).json({message: `${mentorname} Mentor is Not Available Create Before Asigning`})
-            }else if(mentorAvail && !studentAvail){
-                await connection.close();
-                return res.status(504).json({message: `${stuname} Student is Not Available Create Before Asigning`})
-            }else{
-                await collectionstu.updateOne(
-                    { name: stuname },
-                    { $set: { mentor: mentorAvail._id } }
-                  );
-            }
-        }
-        await connection.close();
-        return res.json({ message: `Studenst Assigned to Mentor ${mentorname}` });    
-} catch (err) {
+// Write API to Assign a student to Mentor
+//Select one mentor and Add multiple Student
+app.put("/assingmentor/:mentorobjid", async (req, res) => {
+  const mentorid = req.params.mentorobjid;
+  const students = req.body;
+  const connection = await MongoClient.connect(URL);
+  const db = connection.db("mentorandstudent");
+  try {
+    for (const stuname of students) {
+      const collectionstu = db.collection("students");
+      await collectionstu.updateOne(
+        { _id: new ObjectId(stuname._id) },
+        { $set: { mentor: new ObjectId(mentorid) } }
+      );
+    }
+    await connection.close();
+    return res.json({ message: `Studenst Assigned to Mentor ${mentorid}` });
+  } catch (err) {
     console.log(err);
     res.status(500).json({ message: "somthing went wrong" });
+  }
+});
+// A student who has a mentor should not be shown in List
+app.get("/nonassignedstudents", async (req, res) => {
+  try {
+    const connection = await MongoClient.connect(URL);
+    const db = connection.db("mentorandstudent");
+    const collection = db.collection("students");
+    const nonassignedstudents = await collection
+      .find({ mentor: { $exists: false } })
+      .toArray();
+    await connection.close();
+    res.json(nonassignedstudents);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "somthing went werong" });
+  }
+});
+// Write API to Assign or Change Mentor for particular Student
+// Select One Student and Assign one Mentor
+app.put("/assingonestudent/:studentobjid", async (req, res) => {
+  const studentid = req.params.studentobjid;
+  const selectedmentorid = req.body._id;
+  const connection = await MongoClient.connect(URL);
+  const db = connection.db("mentorandstudent");
+  try {
+    const collectionstu = db.collection("students");
+    await collectionstu.updateOne(
+      { _id: new ObjectId(studentid) },
+      { $set: { mentor: new ObjectId(selectedmentorid) } }
+    );
+    await connection.close();
+    return res.json({
+      message: `Studenst Assigned to Mentor ${selectedmentorid}`,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "somthing went wrong" });
+  }
+});
+//Write API to show all students for a particular mentor
+app.get("/studentsundermentor/:mentorid", async (req, res) => {
+  const mentorid = req.params.mentorid;
+  try {
+    const connection = await MongoClient.connect(URL);
+    const db = connection.db("mentorandstudent");
+    const collection = db.collection("students");
+    const allstudentsparticularmentor = await collection
+      .find({ mentor: new ObjectId(mentorid) })
+      .toArray();
+    await connection.close();
+    res.json(allstudentsparticularmentor);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "somthing went werong" });
+  }
+});
+//Write an API to show the previously assigned mentor for a particular student.
+app.get("/assignedstudent/:studentid", async (req, res) => {
+  const studentid = req.params.studentid;
+  try {
+    const connection = await MongoClient.connect(URL);
+    const db = connection.db("mentorandstudent");
+    const collection = db.collection("students");
+    const assignedstudent = await collection
+      .find({ _id: new ObjectId(studentid) })
+      .toArray();
+    const collectionment = db.collection("mentors");
+    const assignedmentorDetails = await collectionment
+      .find({ _id: new ObjectId(assignedstudent[0].mentor) })
+      .toArray();
+    await connection.close();
+    res.json(assignedmentorDetails);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "somthing went werong" });
   }
 });
